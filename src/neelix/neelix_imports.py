e39e3cc3b2d3050605a3a2d66d3cf0afef9200d1
Ritesh Maheshwari <rmaheshwari@linkedin.com>
2013-07-19 16:44:26 -0700

    Moving stuff

--- src/neelix/neelix_imports.py
+++ src/neelix/neelix_imports.py
@@ -1,15 +1,12 @@
 import os
 import sys
 
-#from neelix.ingraphs_metric import IngraphsMetric
-#from neelix.graphing import jfreechart
 from neelix.graphing import matplotlib_neelix
 
 #Custom metrics
 metric_classes = {
-    #'INGRAPHS' : IngraphsMetric
+    #'MyMetric' : MyMetricParserClass
     }
 graphing_modules = {
-    #'jfreechart' : jfreechart
     'matplotlib' : matplotlib_neelix
     }
