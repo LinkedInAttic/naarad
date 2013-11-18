84ef83d3aed53ed028aaf923d0c8994603fa522e
Sharad Gandhi <sgandhi@linkedin.com>
2013-11-18 15:07:58 -0800

    nose test compatible matplotlib tests

--- test/matplotlib/test_matplotlib.py
+++ test/matplotlib/test_matplotlib.py
@@ -5,14 +5,17 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not us
 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """
-import naarad.graphing.matplotlib_naarad as mpl_n
-from naarad.graphing.plot_data import PlotData as PlotData
 import logging
 import os
 import glob
+import sys
+from nose import *
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'src')))
+
+import naarad.graphing.matplotlib_naarad as mpl_n
+from naarad.graphing.plot_data import PlotData as PlotData
 logger = logging.getLogger('naarad')
-
 
 def validate(filename):
   if os.path.exists(filename):
@@ -23,18 +26,9 @@ def validate(filename):
   else:
     return False
 
-def test_plot(list_of_plots, output_file, scenario_name, validation):
+def plot(list_of_plots, output_file, scenario_name, validation):
   mpl_n.graph_data(list_of_plots, '.', output_file)
-  if validate(output_file + '.png') != validation:
-    logger.error('Plotting of %s with matlplotlib failed', scenario_name)
-  else:
-    logger.info('Plotting of %s with matlplotlib succeeded', scenario_name)
-
-def setup_test():
-  png_list = glob.glob('test*.png')
-  for png in png_list:
-    os.remove(png)
-    logger.info('Deleting : %s', png)
+  assert validate(output_file + '.png') == validation
 
 def init_logging(log_level):
   log_file = 'test_matplotlib.log'
@@ -55,27 +49,29 @@ def init_logging(log_level):
   logger.addHandler(fh)
   logger.addHandler(ch)
 
-def main():
+def setup_matplotlib_tests():
   init_logging('INFO')
-  setup_test()
+  png_list = glob.glob('test*.png')
+  for png in png_list:
+    os.remove(png)
+    logger.info('Deleting : %s', png)
 
-#Test Data
+#Test Cases
+@with_setup(setup_matplotlib_tests())
+def test_generator():
+  #Test Data
   plot1 = PlotData('test1.csv', 1, 'GC Commit', 'MB', None, 600, 1200, 'line')
   plot2 = PlotData('test2.csv', 1, 'GC CMS Pause', 'seconds', None, 600, 1200, 'line')
   plot3 = PlotData('test3.csv', 1, 'GC Promo', 'bps', None, 600, 1200, 'line')
   plot4 = PlotData('test4.csv', 1, 'GC Promo', 'bps', None, 600, 1200, 'line')
   plot5 = PlotData('test5.csv', 1, 'GC Promo', 'bps', None, 600, 1200, 'line')
+  yield plot, [plot1], 'test1', 'single plot with all valid csv', True
+  yield plot, [plot1, plot2], 'test2', 'dual plot with all valid csv', True
+  yield plot, [plot1, plot2, plot3], 'test3', 'multi plot with all valid csv', True
+  yield plot, [plot4], 'test4', 'single plot with 1 empty csv', False
+  yield plot, [plot4, plot1], 'test5', 'dual plot with 1 empty csv', True
+  yield plot, [plot1, plot4, plot3], 'test6', 'multi plot with 1 empty csv', True
+  yield plot, [plot5], 'test4', 'single plot with 1 non-existent csv', False
+  yield plot, [plot5, plot1], 'test5', 'dual plot with 1 non-existent csv', True
+  yield plot, [plot1, plot5, plot3], 'test6', 'multi plot with 1 non-existent csv', True
 
-#Test Cases
-  test_plot([plot1], 'test1', 'single plot with all valid csv', True)
-  test_plot([plot1, plot2], 'test2', 'dual plot with all valid csv', True)
-  test_plot([plot1, plot2, plot3], 'test3', 'multi plot with all valid csv', True)
-  test_plot([plot4], 'test4', 'single plot with 1 empty csv', False)
-  test_plot([plot4, plot1], 'test5', 'dual plot with 1 empty csv', True)
-  test_plot([plot1, plot4, plot3], 'test6', 'multi plot with 1 empty csv', True)
-  test_plot([plot5], 'test4', 'single plot with 1 non-existant csv', False)
-  test_plot([plot5, plot1], 'test5', 'dual plot with 1 non-existant csv', True)
-  test_plot([plot1, plot5, plot3], 'test6', 'multi plot with 1 non-existant csv', True)
-
-
-main()
