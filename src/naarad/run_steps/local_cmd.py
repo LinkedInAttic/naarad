96778322084bbb1f2218ab375033d6044b7c9f46
RiteshMaheshwari <riteshm@gmail.com>
2014-02-05 01:54:13 -0800

    adding run_steps, working code for local cmd

---
+++ src/naarad/run_steps/local_cmd.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+"""
+© 2013 LinkedIn Corp. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+
+import logging
+from naarad.run_types.run_type import Run_Step
+
+logger = logging.getLogger('naarad.run_steps.run_step')
+
+class Local_Cmd(Run_Step):
+
+  def __init__(self, run_type, run_cmd, should_wait=True):
+    Run_Sla.__init__(self, run_type, run_cmd, should_wait)
+
+  def run(self):
+    if self.should_wait
