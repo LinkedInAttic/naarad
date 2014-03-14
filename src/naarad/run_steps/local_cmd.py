756706cc3732f2bafc6dca7c7b0bb53af70eb453
RiteshMaheshwari <riteshm@gmail.com>
2014-03-13 18:04:38 -0700

    adding try catch in kill

--- src/naarad/run_steps/local_cmd.py
+++ src/naarad/run_steps/local_cmd.py
@@ -62,12 +62,16 @@ class Local_Cmd(Run_Step):
     If run_step needs to be killed, this method will be called
     :return: None
     """
-    logger.info('Trying to terminating run_step...')
-    self.process.terminate()
-    time_waited_seconds = 0
-    while self.process.poll() is None and time_waited_seconds < CONSTANTS.SECONDS_TO_KILL_AFTER_SIGTERM:
-      time.sleep(0.5)
-      time_waited_seconds += 0.5
-    if self.process.poll() is None:
-      logger.warning('Waited %d seconds for run_step to terminate. Killing now....', CONSTANTS.SECONDS_TO_KILL_AFTER_SIGTERM )
-      self.process.kill()
+    try:
+      logger.info('Trying to terminating run_step...')
+      self.process.terminate()
+      time_waited_seconds = 0
+      while self.process.poll() is None and time_waited_seconds < CONSTANTS.SECONDS_TO_KILL_AFTER_SIGTERM:
+        time.sleep(0.5)
+        time_waited_seconds += 0.5
+      if self.process.poll() is None:
+        self.process.kill()
+        logger.warning('Waited %d seconds for run_step to terminate. Killing now....', CONSTANTS.SECONDS_TO_KILL_AFTER_SIGTERM )
+    except OSError, e:
+      logger.error('Error while trying to kill the subprocess: %s', e)
+
