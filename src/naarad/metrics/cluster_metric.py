acc9ae581c73dbf39791a813e698333fd8f265b3
zhenyun zhuang <zzhuang@linkedin.com>
2014-03-18 12:31:22 -0700

    updated the comments

--- src/naarad/metrics/cluster_metric.py
+++ src/naarad/metrics/cluster_metric.py
@@ -31,16 +31,16 @@ class ClusterMetric(Metric):
   def __init__ (self, section, aggregate_hosts, aggregate_metrics, metrics, output_directory, resource_path, label, ts_start, ts_end,
                 rule_strings, **other_options):
     self.metrics = metrics
-    self.aggr_metrics = re.split(" ", aggregate_metrics)
-    self.aggr_hosts = re.split(" ", aggregate_hosts) 
+    self.aggr_metrics = aggregate_metrics.split()
+    self.aggr_hosts = aggregate_hosts.split()
                   
     #Metric arguments take 'infile' and 'hostname', for ClusterMetric, they are invalid, so just provide empty strings.     
     Metric.__init__(self, section, '', '', output_directory, resource_path, label, ts_start, ts_end, rule_strings)
     
     # in particular, Section can specify a subset of all rows (default has 43 rows):  "sub_metrics=nr_free_pages nr_inactive_anon"
     for (key, val) in other_options.iteritems():
-      setattr(self, key, val.split())   
-  
+      setattr(self, key, val.split())        
+          
   def collect(self):
     """
     take metrics, filter all metrics based on hostname, and metric_type (which comes from section)
@@ -56,15 +56,12 @@ class ClusterMetric(Metric):
       
       fields = aggr_metric.split(":")   
       cur_metric_type = fields[0].split(".")[0]  # e.g. SAR-device
+      
       if len(fields) > 1:  # if config file has ":sum,count"
         for func in fields[1].split(","):
-          functions.add(func)      
-      
-      function_index = aggr_metric.find(":")  # for use of extracting the sub-metric
-      if function_index > -1:
-        cur_column = aggr_metric[len(cur_metric_type)+1:function_index]  #e.g. sda.await or all.percent-sys
-      else:
-        cur_column = aggr_metric[len(cur_metric_type)+1:]  #e.g. sda.await or all.percent-sys
+          functions.add(func)
+
+      cur_column = '.'.join(fields[0].split('.')[1:])    #e.g. sda.await or all.percent-sys
       cur_column = cur_column.replace('percent-','%')  # to handle the case when user specify "percent-" rather than '%'; we expect "%"
     
       merged_raw = []      #store all the raw values
@@ -98,14 +95,14 @@ class ClusterMetric(Metric):
                 merged_count[ts] += 1
               else:
                 merged_sum[ts] = float(value)
-                merged_count[ts] = 1    
+                merged_count[ts] = 1      
 
       #"raw" csv file
       out_csv = self.get_csv(cur_column)
       with open(out_csv, 'w') as fh:
         self.csv_files.append(out_csv)
-        fh.write("".join(sorted(merged_raw)) )
-        
+        fh.write("".join(sorted(merged_raw)) )        
+      
       #"sum"  csv file
       if 'sum' in functions:
         out_csv = self.get_csv(cur_column + '.sum')
@@ -114,9 +111,9 @@ class ClusterMetric(Metric):
           for k,v in sorted(merged_sum.items()):
             fh.write(k + "," + str(v)+"\n")
       
-      # "aver" csv file  
-      if 'aver' in functions: 
-        out_csv = self.get_csv(cur_column + '.aver')
+      # "avg" csv file  
+      if 'avg' in functions: 
+        out_csv = self.get_csv(cur_column + '.avg')
         self.csv_files.append(out_csv)
         with open(out_csv, 'w') as fh:
           for k,v in sorted(merged_sum.items()):
@@ -131,8 +128,9 @@ class ClusterMetric(Metric):
             fh.write(k + "," + str(v)+"\n")
           
       gc.collect()
-    return True
-  
+    return True    
+ 
+          
   def parse(self):
     """
     merge multiple hosts' csv into one csv file. This approach has the benefit of reusing calculate_stats(), but with the penalty of reading the single csv later for calculate_stats()
---
+++ test/test_cluster_metric.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+"""
+© 2013 LinkedIn Corp. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at  http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+"""
+import os
+import nose  
+import sys
+import uuid
+import shutil
+import time
+# add the path of ~/naarad/src;   the testing py is under ~/naarad/test 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'src')))
+
+#the temporary directory for testing, will remove it after done. 
+tmp_dir = ''
+naarad_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # ~/naarad/
+config_dir = 'examples/conf'
+config_file = 'config-cluster'
+input_dir = 'examples/logs'
+
+files_to_check = ["CLUSTER-1.sda.await.png", "CLUSTER-1.all.percent-sys.png", 'CLUSTER-1.sda.await.sum.png']
+
+def setup():
+  create_tmp_dir()
+
+def teardown():
+  delete_tmp_dir() 
+  #print 'tear down' 
+ 
+def create_tmp_dir():
+  '''create a unique tmp dir to hold the downloaded local files'''
+  ''' if the tmp dir grenerated already exists, then simply return'''
+  ''' the user simply try again to generate another unique tmp dir'''
+  global tmp_dir
+  tmp_dir = os.path.join('./tmp/', str(uuid.uuid4()))
+  if not os.path.exists(tmp_dir):
+    os.makedirs(tmp_dir)
+  else:
+    print "the path of %s already exists, please try again." % tmp_dir
+    return
+  
+def delete_tmp_dir():
+  '''delete the tmp directory'''
+  global tmp_dir
+  shutil.rmtree(tmp_dir)
+  
+def test_cluster():
+  ''' list of abosulute urls with no output file name'''
+  naarad_cmd = os.path.join(naarad_dir, 'bin/naarad')
+  naarad_conf = os.path.join(naarad_dir, config_dir, config_file)
+  naarad_input_dir = os.path.join(naarad_dir, input_dir)
+  
+  command = naarad_cmd + ' -c ' + naarad_conf + ' -i ' + naarad_input_dir + ' -o ' + tmp_dir
+  os.system(command)
+  
+  for fc in files_to_check:
+    file_path = os.path.join(tmp_dir, 'resources' , fc)
+    assert os.path.exists(file_path),  "File of %s does not exist! " % file_path  
