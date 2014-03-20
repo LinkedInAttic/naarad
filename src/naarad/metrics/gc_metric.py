6e86db78a0dd5b525136299309a2d4d8db380930
Sharad Gandhi <sgandhi@linkedin.com>
2014-03-20 13:00:34 -0700

    GC Metric Performance Enhancements

--- src/naarad/metrics/gc_metric.py
+++ src/naarad/metrics/gc_metric.py
@@ -42,7 +42,6 @@ class GCMetric(Metric):
       else:
         setattr(self, key, val)
     self.sub_metric_description = {
-      'appstop' : 'approximate application stop times',
       'gen0' : 'young gen collection time, excluding gc_prologue & gc_epilogue',
       'gen0t' : 'young gen collection time, including gc_prologue & gc_epilogue',
       'gen0usr' : 'young gen collection time in cpu user secs',
@@ -73,66 +72,19 @@ class GCMetric(Metric):
       'usedAfterGC' : 'heap space used memory size (after gc)'
       }
 
-
-  def get_pngname(self, sub_metric):
-    return self.metric_type + '.' + sub_metric + '.png'
-
-  def get_clock_from_jvmts(self, beginning_date, beginning_ts, ts):
-    if beginning_date is None:
-      logger.warning('Returning ts 0 since beginning date is not set')
-      return 0
-    else:
-      diffms = 1000*( float(ts) - beginning_ts )
-      timedelta = datetime.timedelta(milliseconds=diffms)
-      return beginning_date + timedelta
-
-  def parse_val_types(self, sub_metric):
-    outfile = os.path.join(self.resource_directory, self.label + '-' + sub_metric + '-out.txt')
-    if not naarad.utils.is_valid_file(outfile):
-      return
-    outcsv = self.get_csv(sub_metric)
-    with open(outcsv, 'w') as csvf:
-      with open(outfile, 'r') as txt_fh:
-        for line in txt_fh:
-          words = line.split()
-          # Implementing timestamp
-          begin_ts = str( self.get_clock_from_jvmts(self.beginning_date, self.beginning_ts, words[0]) )
-          if self.ts_out_of_range(begin_ts):
-            continue
-          begin_ts = naarad.utils.reconcile_timezones(begin_ts, self.timezone, self.graph_timezone)
-          csvf.write(begin_ts + ',')
-          csvf.write(words[1])
-          csvf.write('\n')
-    self.csv_files.append(outcsv)
-    # os.remove(outfile)
-
   def parse(self):
     prefix = os.path.join(self.resource_directory, self.label)
     awk_cmd = os.path.join(self.bin_path, 'PrintGCStats')
     gc_metrics = set(self.val_types) & set(self.sub_metrics)
-    cmd = awk_cmd + ' -v plot=' + ','.join(gc_metrics) + ' -v splitfiles=1 -v splitfileprefix=' + prefix + ' ' + self.infile
+    if self.ts_start:
+      awk_cmd += ' -v ts_start="' + naarad.utils.get_standardized_timestamp(self.ts_start, None) + '"'
+    if self.ts_end:
+      awk_cmd += ' -v ts_end="' + naarad.utils.get_standardized_timestamp(self.ts_end, None) + '"'
+    cmd = awk_cmd + ' -v plot=' + ','.join(gc_metrics) + ' -v splitfiles=1 -v datestamps=1 -v plotcolumns=2 -v splitfileprefix=' + prefix + ' ' + self.infile
     logger.info("Parsing GC metric with cmd: %s", cmd)
     os.system(cmd)
-    gc_date_regex = re.compile(r'^[0-9]{4}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]+[+-][0-9]{4}:')
-    with open(self.infile, 'r') as inf:
-      for line in inf:
-        if re.match(gc_date_regex, line) and self.beginning_date is None:
-          words = line.split()
-          jvmts = float(words[1].split('.')[0])
-          tstamp = words[0].split('T')
-          time = tstamp[1].split('.')
-          clock = tstamp[0] + ' ' + time[0]
-          self.beginning_date = datetime.datetime.strptime(clock, self.clock_format)
-          self.beginning_ts = float(jvmts)
-          logger.info('Setting beginning date and ts')
-          break
-    threads = []
     for gc_sub_metric in gc_metrics:
-      thread = threading.Thread(target=self.parse_val_types, args=(gc_sub_metric,))
-      thread.start()
-      threads.append(thread)
-    for t in threads:
-      logger.info("Waiting for thread %d to finish.... ", t.ident)
-      t.join()
+      outcsv = self.get_csv(gc_sub_metric)
+      if naarad.utils.is_valid_file(outcsv):
+        self.csv_files.append(outcsv)
     return True
-
