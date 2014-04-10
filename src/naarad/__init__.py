a591c1e64a32decf3f9ffd8fef74ca811a4a626e
Sharad Gandhi <sgandhi@linkedin.com>
2014-04-10 14:13:06 -0700

    Add diff_report_by_location() API

--- src/naarad/__init__.py
+++ src/naarad/__init__.py
@@ -249,6 +249,29 @@ class Naarad(object):
       return CONSTANTS.ERROR
     return CONSTANTS.OK
 
+  def diff_reports_by_location(self, report1_location, report2_location, output_directory, config=None, **kwargs):
+    """
+    Create a diff report using report1 as a baseline
+    :param: report1_location: report to be used as baseline
+    :param: report2_location: report to compare against baseline
+    :param: config file for diff (optional)
+    :param: **kwargs: keyword arguments
+    """
+
+    if kwargs:
+      if 'output_directory' in kwargs.keys():
+        output_directory = kwargs['output_directory']
+    diff_report = Diff([NaaradReport(report1_location, None), NaaradReport(report2_location, None)], 'diff', output_directory, os.path.join(output_directory, self.resource_path), self.resource_path)
+    if config:
+      naarad.utils.extract_sla_from_config_file(diff_report, config)
+    diff_report.generate()
+    if diff_report.sla_failures > 0:
+      return CONSTANTS.SLA_FAILURE
+    if diff_report.status != 'OK':
+      return CONSTANTS.ERROR
+    return CONSTANTS.OK
+
+
   def process_naarad_config(self, config, analysis):
     """
     Process the config file associated with a particular analysis and return metrics, run_steps and crossplots. Also sets output directory and resource_path for an anlaysis
