b3dfe4cedc670149ed06d677c86dffb9e33362b9
RiteshMaheshwari <riteshm@gmail.com>
2014-04-16 10:37:20 -0700

    example of how to use dynamic imports

---
+++ examples/user_defined_modules/user_imports.py
@@ -0,0 +1,8 @@
+metric_classes = {
+    # Add metric label as key and the implemented class name as value here
+    'MYMETRIC' : 'MyMetric'
+    }
+metric_files = {
+    # Add metric label as key and the python file that has the implemented class here
+    'MYMETRIC' : 'my_metric.py'
+    }
