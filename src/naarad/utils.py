888fb46492b1337be0b2229b7b7163a8c252e9c0
RiteshMaheshwari <riteshm@gmail.com>
2014-04-22 14:41:37 -0700

    log warn if predefined metric exists

--- src/naarad/utils.py
+++ src/naarad/utils.py
@@ -47,6 +47,8 @@ def parse_user_defined_metric_classes(config_obj, metric_classes):
     try:
       new_module = imp.load_source(module_name, metric_file)
       new_class = getattr(new_module, metric_class_name)
+      if metric_name in metric_classes.keys():
+        logger.warn('Overriding pre-defined metric class definition for ', metric_name)
       metric_classes[metric_name] = new_class
     except ImportError:
       logger.error('Something wrong with importing a user defined metric class. Skipping metric: ', metric_name)
