99a46d32e1ad8a8c644798e2df5ce17552e3f843
zhenyun zhuang <zzhuang@linkedin.com>
2014-04-28 11:56:46 -0700

    fixed the netstat unit test; removed the log file dependency

--- test/test_netstat_metric.py
+++ test/test_netstat_metric.py
@@ -18,16 +18,55 @@ from naarad.metrics.netstat_metric import NetstatMetric
 #the temporary directory for testing, will remove it after done. 
 tmp_dir = ''
 
+def prepare_data():
+  """
+  Hard code the raw logs and output into files so that netstat metric can pick them up.
+  Doing so can remove the dependency on physical logs.
+  :return:
+  """
+  log = []
+  log.append('2014-04-14 12:09:01.67581	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:01.67581	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:01.67581	tcp        0      500 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:01.67581	tcp        120      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:03.76251	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:03.76251	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:03.76251	tcp        0      200 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:03.76251	tcp        330      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:05.84302	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:05.84302	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:05.84302	tcp        0      345 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:05.84302	tcp        440      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:07.91455	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:07.91455	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:07.91455	tcp        0      0 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:07.91455	tcp        1550      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:09.98031	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:09.98031	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:09.98031	tcp        0      564 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:09.98031	tcp        20      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:12.05993	Active Internet connections (w/o servers)')
+  log.append('2014-04-14 12:09:12.05993	Proto Recv-Q Send-Q Local Address               Foreign Address             State       PID/Program name')
+  log.append('2014-04-14 12:09:12.05993	tcp        0      234 host1.localdomain.com:43214 web1.remotedomain.com:https ESTABLISHED 4996/firefox')
+  log.append('2014-04-14 12:09:12.05993	tcp        3245      0 host1.localdomain.com:48860 email.localdomain.com:https ESTABLISHED 4996/firefox')
+
+  with open(os.path.join(tmp_dir, 'netstat.tcp.out'), 'w') as fh:
+    fh.write('\n'.join(sorted(log)))
+
 def setup():
   create_tmp_dir()
+  prepare_data()
 
 def teardown():
   delete_tmp_dir()
 
 def create_tmp_dir():
-  '''create a unique tmp dir to hold the downloaded local files'''
-  ''' if the tmp dir grenerated already exists, then simply return'''
-  ''' the user simply try again to generate another unique tmp dir'''
+  """
+  create a unique tmp dir to hold the downloaded local files
+  if the tmp_dir grenerated already exists, then simply return
+  the user simply try again to generate another unique tmp dir
+  :return:
+  """
   global tmp_dir
   tmp_dir = os.path.join('./','tmp' + '.' +str(uuid.uuid4()))   #./tmp.'randomstring'
   if not os.path.exists(tmp_dir):
@@ -37,11 +76,17 @@ def create_tmp_dir():
     return
   
 def delete_tmp_dir():
-  '''delete the tmp directory'''
-  global tmp_dir
+  """
+  delete the tmp directory
+  :return:
+  """
   shutil.rmtree(tmp_dir)
   
 def test_netstatmetric():
+  """
+  First construct a NetstatMetric, then call the parse(), finally check whether the output files are there
+  :return:
+  """
   #construct a NetstatMetric
   section = 'NETSTAT-host1'
   label = 'NETSTAT-host1'
@@ -49,16 +94,15 @@ def test_netstatmetric():
   resource_path = 'resources'
   rule_strings = {}
   output_directory = tmp_dir
-  infile=['netstat.tcp.out']
+  infile_list=['netstat.tcp.out']
   other_options = {'connections': 'host1.localdomain.com<->web1.remotedomain.com:https host1:48860<->email', 'processes': '/firefox'}
   ts_start = None
   ts_end = None
   important_sub_metrics =[]
 
-  cur_metric = NetstatMetric(section, infile, hostname, output_directory, resource_path, label, ts_start, ts_end, rule_strings, important_sub_metrics, **other_options)
-  cur_metric.infile_list = [os.path.join('logs', f) for f in cur_metric.infile_list]
+  cur_metric = NetstatMetric(section, infile_list, hostname, output_directory, resource_path, label, ts_start, ts_end, rule_strings, important_sub_metrics, **other_options)
+  cur_metric.infile_list =[os.path.join(tmp_dir, f) for f in cur_metric.infile_list]
 
-  #print cur_metric.infile
   # create sub-directory of resource_path
   sub_dir = os.path.join(output_directory, resource_path)
   if not os.path.exists(sub_dir):
@@ -71,7 +115,7 @@ def test_netstatmetric():
                   'NETSTAT-host1.host1.localdomain.com_43214.web1.remotedomain.com_https.SendQ.csv',
                   'NETSTAT-host1.host1.localdomain.com_48860.email.localdomain.com_https.RecvQ.csv',
                   'NETSTAT-host1.host1.localdomain.com_48860.email.localdomain.com_https.SendQ.csv']
-  for f in output_files: #'raw,avg,sum,count'
+  for f in output_files:
     file_path = os.path.join(sub_dir, f)
     assert os.path.exists(file_path)
 
