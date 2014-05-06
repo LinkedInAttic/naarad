76b17987471a29eab070c3fa77862058b6237baa
Joy <cxiong@linkedin.com>
2014-05-06 10:49:12 -0700

    Homepage redesign

--- src/naarad/metrics/metric.py
+++ src/naarad/metrics/metric.py
@@ -193,6 +193,7 @@ class Metric(object):
           if ts == -1:
             continue
           ts = naarad.utils.reconcile_timezones(ts, self.timezone, self.graph_timezone)
+          print 'JOY timestamp is: ', ts, 'run start time ', self.ts_start, 'run end time ', self.ts_end
           if self.ts_out_of_range(ts):
             continue
           qps[ts.split('.')[0]] += 1
@@ -351,22 +352,40 @@ class Metric(object):
     """
     plot timeseries for sub-metrics 
     """
-    graphed = False
-    for out_csv in self.csv_files:
-      csv_filename = os.path.basename(out_csv)
-      # The last element is .csv, don't need that in the name of the chart
-      column = self.csv_column_map[out_csv]
-      column = naarad.utils.sanitize_string(column)      
-      graph_title = '.'.join(csv_filename.split('.')[0:-1])
-      if self.sub_metric_description and column in self.sub_metric_description.keys():
-        graph_title += ' ('+self.sub_metric_description[column]+')'
-      if self.sub_metric_unit and column in self.sub_metric_unit.keys():
-        plot_data = [PD(input_csv=out_csv, csv_column=1, series_name=graph_title, y_label=column +' ('+ self.sub_metric_unit[column]+')', precision=None, graph_height=600, graph_width=1200, graph_type='line')]
-      else:
-        plot_data = [PD(input_csv=out_csv, csv_column=1, series_name=graph_title, y_label=column, precision=None, graph_height=600, graph_width=1200, graph_type='line')]
-      graphed, div_file = Metric.graphing_modules[graphing_library].graph_data(plot_data, self.resource_directory, self.resource_path, graph_title)
-      if graphed:
-        self.plot_files.append(div_file)
+    if self.groupby:
+      plot_data = {}
+      # plot time series data for submetrics
+      for out_csv in sorted(self.csv_files, reverse=True):
+        csv_filename = os.path.basename(out_csv)
+        # The last element is .csv, don't need that in the name of the chart
+        column = csv_filename.split('.')[-2]
+        transaction_name = ' '.join(csv_filename.split('.')[1:-2])
+        plot = PD(input_csv=out_csv, csv_column=1, series_name=transaction_name + '.' + column, y_label=self.sub_metric_description[column], precision=None, graph_height=500, graph_width=1200, graph_type='line')
+        if transaction_name in plot_data:
+          plot_data[transaction_name].append(plot)
+        else:
+          plot_data[transaction_name] = [plot]
+      for transaction in plot_data:
+        graphed, div_file = Metric.graphing_modules[graphing_library].graph_data(plot_data[transaction], self.resource_directory, self.resource_path, self.label + '.' + transaction )
+        if graphed:
+          self.plot_files.append(div_file)
+    else:
+      graphed = False
+      for out_csv in self.csv_files:
+        csv_filename = os.path.basename(out_csv)
+        # The last element is .csv, don't need that in the name of the chart
+        column = self.csv_column_map[out_csv]
+        column = naarad.utils.sanitize_string(column)
+        graph_title = '.'.join(csv_filename.split('.')[0:-1])
+        if self.sub_metric_description and column in self.sub_metric_description.keys():
+          graph_title += ' ('+self.sub_metric_description[column]+')'
+        if self.sub_metric_unit and column in self.sub_metric_unit.keys():
+          plot_data = [PD(input_csv=out_csv, csv_column=1, series_name=graph_title, y_label=column +' ('+ self.sub_metric_unit[column]+')', precision=None, graph_height=600, graph_width=1200, graph_type='line')]
+        else:
+          plot_data = [PD(input_csv=out_csv, csv_column=1, series_name=graph_title, y_label=column, precision=None, graph_height=600, graph_width=1200, graph_type='line')]
+        graphed, div_file = Metric.graphing_modules[graphing_library].graph_data(plot_data, self.resource_directory, self.resource_path, graph_title)
+        if graphed:
+          self.plot_files.append(div_file)
     return True
   
   def check_important_sub_metrics(self, sub_metric):
