0ddaaff1776a49cff43ea9bac0b37c58aecde198
Sharad Gandhi <sgandhi@linkedin.com>
2014-05-21 11:16:23 -0700

    Add docstrings and update as per comments

--- src/naarad/utils.py
+++ src/naarad/utils.py
@@ -333,28 +333,34 @@ def parse_graph_section(config_obj, section, outdir_default, indir_default):
   return graphing_library, crossplots, outdir_default, indir_default, graph_timezone
 
 def parse_report_section(config_obj, section):
-  kwargs = {}
+  """
+  parse the [REPORT] section of a config file to extract various reporting options to be passed to the Report object
+  :param: config_obj : configparser object for the config file passed in to naarad
+  :param: section: name of the section. 'REPORT' should be passed in here
+  :return: report_kwargs: dictionary of Reporting options and values specified in config.
+  """
+  report_kwargs = {}
   if config_obj.has_option(section, 'stylesheet_includes'):
-    kwargs['stylesheet_includes'] = config_obj.get(section, 'stylesheet_includes')
+    report_kwargs['stylesheet_includes'] = config_obj.get(section, 'stylesheet_includes')
   if config_obj.has_option(section, 'javascript_includes'):
-    kwargs['javascript_includes'] = config_obj.get(section, 'javascript_includes')
+    report_kwargs['javascript_includes'] = config_obj.get(section, 'javascript_includes')
   if config_obj.has_option(section, 'header_template'):
-    kwargs['header_template'] = config_obj.get(section, 'header_template')
+    report_kwargs['header_template'] = config_obj.get(section, 'header_template')
   if config_obj.has_option(section, 'footer_template'):
-    kwargs['footer_template'] = config_obj.get(section, 'footer_template')
+    report_kwargs['footer_template'] = config_obj.get(section, 'footer_template')
   if config_obj.has_option(section, 'summary_content_template'):
-    kwargs['summary_content_template'] = config_obj.get(section, 'summary_content_template')
+    report_kwargs['summary_content_template'] = config_obj.get(section, 'summary_content_template')
   if config_obj.has_option(section, 'summary_page_template'):
-    kwargs['summary_page_template'] = config_obj.get(section, 'summary_page_template')
+    report_kwargs['summary_page_template'] = config_obj.get(section, 'summary_page_template')
   if config_obj.has_option(section, 'metric_page_template'):
-    kwargs['metric_page_template'] = config_obj.get(section, 'metric_page_template')
+    report_kwargs['metric_page_template'] = config_obj.get(section, 'metric_page_template')
   if config_obj.has_option(section, 'client_charting_template'):
-    kwargs['client_charting_template'] = config_obj.get(section, 'client_charting_template')
+    report_kwargs['client_charting_template'] = config_obj.get(section, 'client_charting_template')
   if config_obj.has_option(section, 'diff_client_charting_template'):
-    kwargs['diff_client_charting_template'] = config_obj.get(section, 'diff_client_charting_template')
+    report_kwargs['diff_client_charting_template'] = config_obj.get(section, 'diff_client_charting_template')
   if config_obj.has_option(section, 'diff_page_template'):
-    kwargs['diff_page_template'] = config_obj.get(section, 'diff_page_template')
-  return kwargs
+    report_kwargs['diff_page_template'] = config_obj.get(section, 'diff_page_template')
+  return report_kwargs
 
 def reconcile_timezones(begin_ts, ts_timezone, graph_timezone):
   if not graph_timezone:
