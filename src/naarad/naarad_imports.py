bbb5c4df38d597b714691c6dbb0d8577a4c77ca4
Ritesh Maheshwari <rmaheshwari@linkedin.com>
2014-05-27 15:51:02 -0700

    try catch on matplotlib

--- src/naarad/naarad_imports.py
+++ src/naarad/naarad_imports.py
@@ -5,7 +5,11 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not us
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 """
 
-from naarad.graphing import matplotlib_naarad
+try:
+  from naarad.graphing import matplotlib_naarad
+except ImportError:
+  no_matplotlib = True
+
 from naarad.graphing import pygal_naarad
 from naarad.metrics.linkedin_android_rum_metric import LinkedInAndroidRumMetric
 from naarad.metrics.gc_metric import GCMetric
@@ -38,7 +42,12 @@ aggregate_metric_classes = {
     'CLUSTER' : ClusterMetric,
     }    
     
-graphing_modules = {
+if no_matplotlib:
+  graphing_modules = {
+    'svg': pygal_naarad
+    }
+else:
+  graphing_modules = {
     'matplotlib': matplotlib_naarad,
     'svg': pygal_naarad
     }
