a0cffa6028e287e921c698a4b5ca8729787b7150
Ritesh Maheshwari <rmaheshwari@linkedin.com>
2014-06-03 19:16:02 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 
-import os
+#import os
 from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
@@ -11,10 +11,9 @@ setup(name="naarad",
       url='https://github.com/linkedin/naarad',
       version='1.0',
       packages=['naarad', 'naarad.metrics', 'naarad.graphing', 'naarad.reporting', 'naarad.run_steps', 'naarad.resources'],  
-      #packages = find_packages(),
       scripts = ['bin/naarad', 'bin/PrintGCStats'],
       package_dir={ '' : 'src'},
-      #package_data={ '' : ['src/naarad/resources/*.html']},
+      package_data={ '' : ['src/naarad/resources/*.html']},
       include_package_data=True,
       install_requires=required,
       )
