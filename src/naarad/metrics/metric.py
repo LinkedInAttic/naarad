0d7cec514c01706d4d361a64ffe0eaec0a16b6eb
Joy <cxiong@linkedin.com>
2014-07-23 15:01:11 -0700

    Bug fix for summary table

--- src/naarad/metrics/metric.py
+++ src/naarad/metrics/metric.py
@@ -324,6 +324,12 @@ class Metric(object):
         self.update_summary_stats(column)
 
   def calculate_stats(self):
+    """
+    Calculate stats with different function depending on the metric type:
+    Data is recorded in memory for base metric type, and use calculate_base_metric_stats()
+    Data is recorded in CSV file for other metric types, and use calculate_other_metric_stats()
+
+    """
     metric_type = self.metric_type.split('-')[0]
     if metric_type in naarad.naarad_imports.metric_classes or metric_type in naarad.naarad_imports.aggregate_metric_classes:
       self.calculate_other_metric_stats()
@@ -367,7 +373,7 @@ class Metric(object):
 
   def calculate_other_metric_stats(self):
     stats_to_calculate = ['mean', 'std', 'min', 'max']  # TODO: get input from user
-    percentiles_to_calculate = range(0, 100, 1)  # TODO: get input from user
+    percentiles_to_calculate = range(0, 101, 1)  # TODO: get input from user
     headers = CONSTANTS.SUBMETRIC_HEADER + ',mean,std,p50,p75,p90,p95,p99,min,max\n'  # TODO: This will be built from user input later on
     metric_stats_csv_file = self.get_stats_csv()
     imp_metric_stats_csv_file = self.get_important_sub_metrics_csv()
