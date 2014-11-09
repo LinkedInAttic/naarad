a1063beec342b56165518aa08276e3365b198627
Richard Hsu <rhsu@linkedin.com>
2014-11-09 01:17:16 -0800

    Adding interrupts, netstat, per core for cpuusage and cpuhz and top -c toggle.

--- bin/naarad_metric_collector.sh
+++ bin/naarad_metric_collector.sh
@@ -19,18 +19,18 @@ fi
 mkdir -p $RESULT
 
 #####################
-# EVERY 2 SECONDS
+# EVERY INTERVAL SECONDS
 #####################
 echo $t >> $RESULT/top.out &
-top -b -n $COUNT -d $INTERVAL | grep -A 40 '^top' >> $RESULT/top.out &
+top -b -c -n $COUNT -d $INTERVAL | grep -A 40 '^top' >> $RESULT/top.out &
 sar -B $INTERVAL $COUNT >> $RESULT/sar.paging.out &
 sar -d -p $INTERVAL $COUNT >> $RESULT/sar.device.out &
 sar -R $INTERVAL $COUNT >> $RESULT/sar.memory.out &
 sar -r $INTERVAL $COUNT >> $RESULT/sar.memutil.out &
-sar -u ALL $INTERVAL $COUNT >> $RESULT/sar.cpuusage.out &
+sar -u ALL -P ALL $INTERVAL $COUNT >> $RESULT/sar.cpuusage.out &
 sar -n DEV $INTERVAL $COUNT >> $RESULT/sar.network.out &
 sar -W $INTERVAL $COUNT >> $RESULT/sar.swapping.out &
-sar -m $INTERVAL $COUNT >> $RESULT/sar.cpuhz.out &
+sar -m -P ALL $INTERVAL $COUNT >> $RESULT/sar.cpuhz.out &
 sar -n EDEV $INTERVAL $COUNT >> $RESULT/sar.edev.out &
 sar -n TCP $INTERVAL $COUNT >> $RESULT/sar.tcp.out &
 sar -n ETCP $INTERVAL $COUNT >> $RESULT/sar.etcp.out &
@@ -39,10 +39,12 @@ sar -w $INTERVAL $COUNT >> $RESULT/sar.switching.out &
 sar -q $INTERVAL $COUNT >> $RESULT/sar.queue.out &
 
 #####################
-# EVERY 2 SECONDS (for now)
+# EVERY INTERVAL SECONDS
 #####################
 COUNT1=$COUNT
 INTERVAL1=$INTERVAL
 for ((i=1; i<= $COUNT1; i++)); do cat /proc/meminfo | sed "s/^/$(date +%Y-%m-%d\ %H:%M:%S.%05N)\t/" >> $RESULT/proc.meminfo.out; sleep $INTERVAL1 ; done &
 for ((i=1; i<= $COUNT1; i++)); do cat /proc/vmstat | sed "s/^/$(date +%Y-%m-%d\ %H:%M:%S.%05N)\t/" >> $RESULT/proc.vmstat.out; sleep $INTERVAL1 ; done &
 for ((i=1; i<= $COUNT1; i++)); do cat /proc/zoneinfo | sed "s/^/$(date +%Y-%m-%d\ %H:%M:%S.%05N)\t/" >> $RESULT/proc.zoneinfo.out; sleep $INTERVAL1 ; done &
+for ((i=1; i<= $COUNT1; i++)); do cat /proc/interrupts | sed "s/^/$(date +%Y-%m-%d\ %H:%M:%S.%05N)\t/" >> $RESULT/proc.interrupts.out; sleep $INTERVAL1 ; done &
+for ((i=1; i<= $COUNT1; i++)); do netstat -s | sed "s/^/$(date +%Y-%m-%d\ %H:%M:%S.%05N)\t/" >> $RESULT/netstat.out; sleep $INTERVAL1 ; done &
