1f4d39a29919cb7b2fda2038c0d6ba4547fabc14
Richard Hsu <rhsu@linkedin.com>
2015-04-22 14:48:38 -0700

    Updated src/naarad/resources for flake8 fixes

--- src/naarad/resources/__init__.py
+++ src/naarad/resources/__init__.py
@@ -1,5 +1,6 @@
 import pkg_resources
 
+
 def get_dir():
   """Return the location of resources for report"""
-  return pkg_resources.resource_filename('naarad.resources',None)
+  return pkg_resources.resource_filename('naarad.resources', None)
