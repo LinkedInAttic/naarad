8464cb54cc2a182a44be8e61bf5343866bff6162
Richard Hsu <rhsu@linkedin.com>
2015-06-04 13:40:56 -0700

    Update utils for auto discovery to just have empty aggr metrics

--- src/naarad/utils.py
+++ src/naarad/utils.py
@@ -921,7 +921,7 @@ def discover_by_name(input_directory, output_directory):
   log_files = os.listdir(input_directory)
   for log_file in log_files:
     if log_file in CONSTANTS.SUPPORTED_FILENAME_MAPPING.keys():
-      metric_list.append(initialize_metric(CONSTANTS.SUPPORTED_FILENAME_MAPPING[log_file], [log_file], None, output_directory, CONSTANTS.RESOURCE_PATH,
+      metric_list.append(initialize_metric(CONSTANTS.SUPPORTED_FILENAME_MAPPING[log_file], [log_file], None, [], output_directory, CONSTANTS.RESOURCE_PATH,
                                            CONSTANTS.SUPPORTED_FILENAME_MAPPING[log_file], None, None, {}, None, None, {}))
     else:
       logger.warning('Unable to determine metric type for file: %s', log_file)
